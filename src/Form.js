7baa9644e0c51f262917b55b3bf1627f2b74691a
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-11 10:49:59 +0800

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 export default function Form() {
-  const [firstName, setFirstName] = React.useState('');
+  //const [firstName, setFirstName] = React.useState('');
+  const [formData, setFormData] = React.useState({
+    firstName: '',
+    lastName: '',
+  });
 
   function handleChange(event) {
-    console.log(event);
+    console.log(event.target.value);
   }
+
+  //console.log(firstName);
 
   return (
     <form>
-      <input type="text" placeholder="First Name" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      />
     </form>
   );
 }
