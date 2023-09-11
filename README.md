f24ecb720ecd602bad894398ac6fbfcd4d6f3262
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-11 09:48:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ylhdqe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ylhdqe)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const user = {
+
+  name : 'testing'
+  
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
