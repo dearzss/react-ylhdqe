3549c265450d25a72fc7f9f536e325d1f84e5485
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-11 15:05:40 +0800

    1 file modified

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './signup.css';
 
-function handleSubmit(event) {
-  event.preventDefault();
-}
-
-function handleChange(event) {
-  console.log(event.target);
-}
-
 export default function SignUp() {
+  const [formData, setFormData] = React.useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    joinedNewsletter: true,
+  });
+
+  console.log(formData);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    /*setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === 'checkbox' ? checked : value,
+    }));*/
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === 'checkbox' ? checked : value,
+      };
+    });
+  }
+
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
@@ -19,6 +38,7 @@ export default function SignUp() {
           className="form-input"
           name="email"
           onChange={handleChange}
+          value={formData.email}
         />
         <input
           type="password"
@@ -26,6 +46,7 @@ export default function SignUp() {
           className="form-input"
           name="password"
           onChange={handleChange}
+          value={formData.password}
         />
         <input
           type="password"
@@ -33,6 +54,7 @@ export default function SignUp() {
           className="form-input"
           name="passwordConfirm"
           onChange={handleChange}
+          value={formData.passwordConfirm}
         />
         <div>
           <input
@@ -40,9 +62,11 @@ export default function SignUp() {
             type="checkbox"
             name="joinedNewsletter"
             onChange={handleChange}
+            checked={formData.joinedNewsletter}
           />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
+        <button className="form-submit">Sign Up</button>
       </form>
     </div>
   );
