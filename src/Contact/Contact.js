c57877d8fc6927971c680bd4e9a0f20b0e711e27
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-13 16:26:46 +0800

    1 file modified, 1 added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ContactList from './ContactList.js';
+
+const initialContacts = [
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' },
+];
 
 export default function Contact() {
-  return <div>2</div>;
+  return <ContactList />;
 }
