3785749ad4dbdeec66c43d1e9ebc42c43ab85079
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-18 16:03:08 +0800

    1 file modified, 2 added

--- src/Die/AppDie.js
+++ src/Die/AppDie.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Die from './Die.js';
+import './Die.css';
 
 export default function AppDie() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  console.log(dice);
+
   return (
-    <>
-      <h1>1</h1>
-    </>
+    <main>
+      <div className="dice-container">
+        {dice.map((die) => (
+          <Die value={die} />
+        ))}
+      </div>
+      <button 
+        onClick={() => {
+          setDice(allNewDice());
+        }}
+      >
+        Roll
+      </button>
+    </main>
   );
 }
