30196bb789e418456ab6a007913031928a71c4fd
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-18 14:46:32 +0800

    1 file modified

--- src/UseEffect.js
+++ src/UseEffect.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 export default function UseEffect() {
   const [count, setCount] = React.useState(0);
+  const [calculation, setCalculation] = React.useState(0);
+
   React.useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  }, []);
+    setCalculation(() => count * 2);
+  }, [count]);
   /*
   -> no [] means runs on every render
   -> [] means Only run the effect on the initial render
-
+  -> [count] means if the count variable updates, the effect will run again
   */
 
-  return <h1>I've rendered {count} times!</h1>;
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
 }
