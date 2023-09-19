052158206bfba6a34808deae4ef24cf44f20d6b1
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-19 11:30:30 +0800

    1 file modified

--- src/Die/AppDie.js
+++ src/Die/AppDie.js
@@ -87,3 +87,21 @@ export default function AppDie() {
     </>
   );
 }
+
+function getCurrentDate(separator = '-') {
+  let newDate = new Date();
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+
+  let curTime =
+    newDate.getHours() +
+    ':' +
+    newDate.getMinutes() +
+    ':' +
+    newDate.getSeconds();
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date}${' '}${curTime}`;
+}
