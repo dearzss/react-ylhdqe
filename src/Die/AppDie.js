d43689ce8d658efa8d06453e54b679287aa77f91
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-19 11:20:04 +0800

    3 files modified

--- src/Die/AppDie.js
+++ src/Die/AppDie.js
@@ -1,23 +1,71 @@
 import React from 'react';
 import Die from './Die.js';
 import './Die.css';
+import { nanoid } from 'nanoid';
 
 export default function AppDie() {
+  const [dices, setDices] = React.useState(allNewDice());
+
+  function generateNewDice() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDice());
+    }
+    return newDice;
+  }
+
+  function onHeld(heldId) {
+    setDices(
+      dices.map((dice) => {
+        if (dice.id === heldId) {
+          return {
+            ...dice,
+            isHeld: !dice.isHeld,
+          };
+        } else return dice;
+      })
+    );
+  }
+
+  function rollDice() {
+    setDices(
+      dices.map((dice) => {
+        if (dice.isHeld) {
+          return dice;
+        } else {
+          return generateNewDice();
+        }
+      })
+    );
+  }
+
   return (
     <>
       <main>
         <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
+          {dices.map((dice) => (
+            <Die
+              value={dice.value}
+              key={dice.id}
+              isHeld={dice.isHeld}
+              onClick={() => {
+                onHeld(dice.id);
+              }}
+            />
+          ))}
         </div>
+        <br />
+        <button className="dice-button" onClick={rollDice}>
+          Roll
+        </button>
       </main>
     </>
   );
--- src/Die/Die.js
+++ src/Die/Die.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export default function (props) {
+  const styles = {
+    backgroundColor: props.isHeld ? '#59E391' : 'white',
+  };
+
   return (
-    <div className="die-face">
+    <div
+      className="die-face"
+      style={styles}
+      onClick={(e) => {
+        props.onClick();
+      }}
+    >
       <h2 className="die-number">{props.value}</h2>
     </div>
   );
