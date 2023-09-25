c5ab55f9fd773d3a4dfc9a50f9d827960430e12f
dearzss <53515744+dearzss@users.noreply.github.com>
2023-09-25 11:40:17 +0800

    1 file modified, 1 added

--- src/SearchParams/SearchParams.jsx
+++ src/SearchParams/SearchParams.jsx
@@ -6,13 +6,13 @@ import {
   Link,
   useSearchParams,
 } from 'react-router-dom';
-import HomePage from './HomePage.jsx';
+import HomePageLayout from './HomePageLayout.jsx';
 
 export default function SearchParams() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/characters" element={<HomePage />} />
+        <Route path="/characters" element={<HomePageLayout />} />
       </Routes>
     </BrowserRouter>
   );
